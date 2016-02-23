174f0652033d9f7c55a44a3a467713ddcae0c8c9
Josh Wu <joshwu@me.com>
2016-02-22 18:47:46 -0800

    fix filestream to open files and to check if stream is open

--- journal.cpp
+++ journal.cpp
@@ -1,14 +1,20 @@
+/*
+ *
+ * Simple Program intended to create template for journals
+ * and automatically open them in sublime after creation
+ * with cursor at the required location. Automatically logs
+ * timestamp of the entry.
+ */
+
 #include <iostream>
 #include <ctime>
 #include <fstream>
 #include <string.h>
 #include <sys/stat.h>
+#include <exception>
 using namespace std;
+
 /*
- * Simple Program intended to create template for journals
- * and automatically open them in sublime after creation
- * with cursor at the required location. Automatically logs
- * timestamp of the entry.
  * REFERENCE FOR MY PURPOSES
  * Sublime accepts argument ':line-number:column-number' to indicate position for the cursor.
  */
@@ -45,6 +51,11 @@ bool validate_date(char const date_to_validate[]){
  */
 int main(int argc, char const *argv[])
 {
+
+	std::fstream test_stream;
+	test_stream.open("/User/Josh/Downloads/test.txt", std::ios::out);
+	test_stream << "Amazing journal" << std::endl;
+	test_stream.close();
 	time_t raw_time = time(NULL);
 	struct tm target_time = *localtime(&raw_time); //Date which is to be written about, defaults to current time
 	struct tm journal_time = *localtime(&raw_time);
@@ -72,16 +83,8 @@ int main(int argc, char const *argv[])
 	target_time = journal_time; //Making target_time and journal time equivalent
 	//check for existing journal for specified week (containing date)
 	char file_name[] = "1970-01-01 Journal.md";
-	std::ifstream fs;
-	// for (int i = 0; i < 7 && !fs.is_open; ++i)
-	// {
-	// 	//Go through the current day and seven days plus to check for existing journal
-	// 	journal_time.tm_mday += i;
-	// 	mktime(&journal_time);
-	// 	strftime(file_name, strlen("1970-01-01 Journal.md"), "%Y-%M-%D Journal.md", &journal_time);
-	// 	journal_time.tm_mday -= i;
-	// }
-	//Find difference between target time and next sunday
+	std::fstream fs;
+
 	if (journal_time.tm_wday != 0)
 	{
 		//Journal time is now set to the journal name
@@ -90,19 +93,35 @@ int main(int argc, char const *argv[])
 	}
 	//create file name for next sunday
 	strftime(file_name, strlen("1970-01-01 Journal.md"), "%F Journal.md", &journal_time);
-	char path[256] = "/Users/Josh/Google\\ Drive/Informal/Journal/";
+	/*
+	 * PATH! Edit this for your computer. Note that spaces **DO NOT NEED TO BE ESCAPED**
+	 */
+	char path[256] = "/Users/Josh/Google Drive/Informal/Journal/";
 	strcat(path, file_name);
+	/*
+	 *
+	 */
 
 	//Check if file exists
 	struct stat buf;
 	if (stat(path, &buf) == -1)
 	{
 		//File does not exist
-		std::cout << path << " does not exist. Attempting to create file." << std::endl;
+		std::cout << path << " does not exist. Creating new file." << std::endl;
+		fs.open(path, std::ios::out);
+		fs.close();
 	}
-	std::cout << "Attempting to open path: " << path << std::endl;
-	fs.open(path, ios::in | ios::out);
+	std::cout << "Attempting to open path: \"" << path << "\"" << std::endl << std::endl;
+	fs.open(path, std::ios::in | std::ios::out | std::ios::app);
+
+	if (!fs.is_open())
+	{
+		std::cout << "Something has happened! :S" << std::endl 
+		<< "File could not be opened" << std::endl;
+		throw std::runtime_error("File could not be opened\n");
+	}
 	// write current date/time to journal after date entry
 	// open in sublime with cursor at line below logging
+	fs.close();
 	return 0;
 }
